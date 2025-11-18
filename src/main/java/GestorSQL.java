b92154a5c1567bc5151ea1868ae07a1b131c9df1
DanielGS11 <DanielGS11>
2025-11-18 18:35:39 +0100

    Commit 3: RegistrarPropietario()[Finalizado], RegistrarCoche(), ImportarCSV()

--- src/main/java/GestorSQL.java
+++ src/main/java/GestorSQL.java
@@ -5,6 +5,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
@@ -56,7 +57,7 @@ public class GestorSQL {
         }
     }
 
-    public void InicializarTabla() {
+    public void InicializarTablas() {
         if (!comprobarConexion()) {
             return;
         }
@@ -82,6 +83,7 @@ public class GestorSQL {
                         "marca VARCHAR(50) NOT NULL, " +
                         "modelo VARCHAR(50) NOT NULL, " +
                         "extras VARCHAR(255), " +
+                        "precio DECIMAL(10, 2), " +
                         "id_propietario INT," +
                         "FOREIGN KEY (id_propietario) REFERENCES propietarios(id_propietario)" +
                         ");"
@@ -118,6 +120,7 @@ public class GestorSQL {
                         "marca VARCHAR(50) NOT NULL, " +
                         "modelo VARCHAR(50) NOT NULL, " +
                         "extras VARCHAR(255), " +
+                        "precio DECIMAL(10, 2), " +
                         "id_propietario INTEGER," +
                         "FOREIGN KEY (id_propietario) REFERENCES propietarios(id_propietario)" +
                         ");"
@@ -147,8 +150,37 @@ public class GestorSQL {
             return;
         }
 
+        boolean camposSinRellenar = false;
+        if (dni.isEmpty()) {
+            System.out.println("Por favor, rellene el campo de DNI");
+            camposSinRellenar = true;
+        }
+        if (name.isEmpty()) {
+            System.out.println("Por favor, rellene el campo de Nombre");
+            camposSinRellenar = true;
+        }
+        if (apellidos.isEmpty()) {
+            System.out.println("Por favor, rellene el campo de Apellidos");
+            camposSinRellenar = true;
+        }
+
+        if (camposSinRellenar) {
+            return;
+        }
+
         try {
-            Statement comprobarPropietarios  = conexion
+            PreparedStatement comprobarPropietarios  = conexion.prepareStatement("SELECT * FROM propietarios WHERE dni= ? ");
+            comprobarPropietarios.setString(1, dni);
+
+            ResultSet rs = comprobarPropietarios.executeQuery();
+
+            while (rs.next()) {
+                if (rs.getString("dni").equalsIgnoreCase(dni)) {
+                    System.out.printf("Ya existe un propietario con el dni '%s'\n", dni);
+                    return;
+                }
+            }
+
             PreparedStatement propietario = conexion.prepareStatement("INSERT INTO propietarios(dni, nombre, apellidos, telefono) VALUES (?, ?, ?, ?);");
 
             propietario.setString(1, dni);
@@ -157,6 +189,140 @@ public class GestorSQL {
             propietario.setString(4, telefono.isEmpty() ? null : telefono);
 
             propietario.executeUpdate();
+
+            System.out.println("Propietario insertado correctamente");
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+
+    public void registrarCoche(String matricula, String marca, String modelo, List<String> extras, double precio) {
+        if (!comprobarConexion()) {
+            return;
+        }
+
+        boolean camposSinRellenar = false;
+        if (matricula.isEmpty()) {
+            System.out.println("Por favor, rellene el campo de Matricula");
+            camposSinRellenar = true;
+        }
+        if (marca.isEmpty()) {
+            System.out.println("Por favor, rellene el campo de Marca");
+            camposSinRellenar = true;
+        }
+        if (modelo.isEmpty()) {
+            System.out.println("Por favor, rellene el campo de Modelo");
+            camposSinRellenar = true;
+        }
+        if (precio <= 0) {
+            System.out.println("Por favor, rellene el campo de Precio");
+            camposSinRellenar = true;
+        }
+
+        if (camposSinRellenar) {
+            return;
+        }
+
+        try {
+            PreparedStatement comprobarCocheDuplicado  = conexion.prepareStatement("SELECT * FROM coches WHERE matricula= ? ");
+            comprobarCocheDuplicado.setString(1, matricula);
+
+            ResultSet rs = comprobarCocheDuplicado.executeQuery();
+
+            while (rs.next()) {
+                if (rs.getString("matricula").equalsIgnoreCase(matricula)) {
+                    System.out.printf("Ya existe un coche con la matricula '%s'\n", matricula);
+                    return;
+                }
+            }
+
+            PreparedStatement coche = conexion.prepareStatement("INSERT INTO coches(matricula, marca, modelo, extras, precio) VALUES (?, ?, ?, ?, ?);");
+
+            coche.setString(1, matricula);
+            coche.setString(2, marca);
+            coche.setString(3, modelo);
+            coche.setString(4, extras.isEmpty() ? null : extras.toString());
+            coche.setDouble(5, precio);
+
+            coche.executeUpdate();
+
+            System.out.println("Coche insertado correctamente");
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public void importarCSV() {
+        if (!comprobarConexion()) {
+            return;
+        }
+
+        cargarProperties();
+        leerCSV();
+
+        System.out.printf("Importando '%s' a la base de datos...\n", propiedades.get(2));
+
+        try {
+            conexion.setAutoCommit(false);
+            PreparedStatement coche = conexion.prepareStatement("INSERT INTO coches(matricula, marca, modelo, extras, precio) VALUES (?, ?, ?, ?, ?);");
+
+            for (String s : lineasCSV) {
+                List<String> datosCoches = new ArrayList<>(List.of(s.trim().split(";")));
+
+                if (datosCoches.size() == 4) {
+                    datosCoches.add("null");
+                }
+
+                StringBuilder extras = new StringBuilder();
+                Arrays.stream(datosCoches.get(4).trim().split("[|]")).toList()
+                        .forEach(e -> extras.append(e).append(", "));
+
+                extras.deleteCharAt(extras.lastIndexOf(","));
+
+                coche.setString(1, datosCoches.getFirst().trim());
+                coche.setString(2, datosCoches.get(1).trim());
+                coche.setString(3, datosCoches.get(2).trim());
+                coche.setString(4, (extras.toString().trim().equals("null") ? null : extras.toString()));
+                coche.setDouble(5, Double.parseDouble(datosCoches.get(3).trim()));
+
+                coche.executeUpdate();
+            }
+
+            conexion.commit();
+            System.out.printf("Importacion de '%s' completada exitosamente\n", propiedades.get(2));
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+
+            try {
+                conexion.rollback();
+                System.out.printf("Importacion cancelada con exito. Compruebe que no haya ningun coche en '%s' " +
+                                "cuya matricula exista en la base de datos.\n", propiedades.get(2));
+            } catch (SQLException ex) {
+                System.out.println("Error: " + ex.getMessage());
+            }
+        } finally {
+            try {
+                conexion.setAutoCommit(true);
+            } catch (SQLException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
+
+    public void limpiarTablas() {
+        if (!comprobarConexion()) {
+            return;
+        }
+
+        try {
+            Statement limpiarTablas = conexion.createStatement();
+
+            limpiarTablas.addBatch("DELETE FROM propietarios;");
+            limpiarTablas.addBatch("DELETE FROM coches;");
+            limpiarTablas.addBatch("DELETE FROM traspasos;");
+
+            limpiarTablas.executeBatch();
         } catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
@@ -192,6 +358,7 @@ public class GestorSQL {
             try {
                 cargarProperties();
                 lineasCSV = Files.readAllLines(Paths.get(propiedades.get(2)));
+                lineasCSV.removeFirst();
             } catch (IOException e) {
                 System.out.println("Error: " + e.getMessage());
             }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,29 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         GestorSQL gestorSQL = new GestorSQL();
 
-        gestorSQL.ConectarBBDD("1");
-        gestorSQL.InicializarTabla();
 
-        gestorSQL.registrarPropietario("fhdjk", "dani", "garcia", "");
-        gestorSQL.registrarPropietario("fhdjk", "dani", "garcia", "");
+        gestorSQL.ConectarBBDD("1");
+        gestorSQL.InicializarTablas();
+        gestorSQL.limpiarTablas();
+
+        gestorSQL.registrarPropietario("fhdjk", "", "garcia", "");
+        gestorSQL.registrarCoche("8530", "fjsal", "hfao", new ArrayList<>(), 100.70);
+        gestorSQL.registrarCoche("8530", "fjsal", "hfao", new ArrayList<>(), 100.70);
+        gestorSQL.importarCSV();
+
+        System.out.println("\n----------------------------------------------------------------------------------------\n");
 
         gestorSQL.ConectarBBDD("2");
-        gestorSQL.InicializarTabla();
+        gestorSQL.InicializarTablas();
+        gestorSQL.limpiarTablas();
 
         gestorSQL.registrarPropietario("fhdjk", "dani", "garcia", "");
-        gestorSQL.registrarPropietario("fhdjk", "dani", "garcia", "");
+        gestorSQL.registrarCoche("8530", "fjsal", "hfao", new ArrayList<>(), 100.70);
+        gestorSQL.registrarCoche("8530", "fjsal", "hfao", new ArrayList<>(), 100.70);
+        gestorSQL.importarCSV();
+
     }
 }
